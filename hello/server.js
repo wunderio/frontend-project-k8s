be9172513f0f03641e6877ba48e43a6d39e29ef3
Jānis Bebrītis <janis.bebritis@wunder.io>
2019-11-07 12:06:18 +0200

    url prefix for service containers is mandatory

--- hello/server.js
+++ hello/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/hello', (req, res) => res.send('Hello'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- world/server.js
+++ world/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-app.get('/', (req, res) => res.send('World'))
+app.get('/world', (req, res) => res.send('World'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
