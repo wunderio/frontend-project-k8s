837aa9a5e22392f7f34a2c817bbd5cef52b5dea6
Florian Lorétan <florian.loretan@wunder.io>
2019-11-12 17:20:56 +0100

    Get information from Elasticsearch into our service.

--- hello/server.js
+++ hello/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000;
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: `${process.env}:9200` })
 
-app.get('/hello', (req, res) => res.send('Hello'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/hello', (req, res) => res.send('Hello'));
+
+app.get('/hello/elasticsearch-status', async (req, res) => {
+  const health = await client.cluster.health();
+  res.send(health.status);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
