c0624a45a3b9c54a01bae877db6e47d085ba174b
Florian Lorétan <florian.loretan@wunder.io>
2019-11-12 17:35:04 +0100

    Add missing protocol to Elasticsearch URL.

--- hello/server.js
+++ hello/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000;
 const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: `${process.env.ELASTICSEARCH_HOST}:9200` })
+const client = new Client({ node: `http://${process.env.ELASTICSEARCH_HOST}:9200` })
 
 
 app.get('/hello', (req, res) => res.send('Hello'));
