f5c87a5ae1637e51404e09db2ee1ed527be4a8ab
Florian Lorétan <florian.loretan@wunder.io>
2019-11-12 15:48:28 +0100

    Write to files from cron and serve them.

--- world/server.js
+++ world/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-app.get('/world', (req, res) => res.send('World'))
+app.get('/world', (req, res) => res.send('World'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/world/files', express.static('files'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
