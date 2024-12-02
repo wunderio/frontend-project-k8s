c47abf474da33a261e347c3d55feba1d451168d8
Jānis Bebrītis <janis.bebritis@wunder.io>
2024-12-02 16:18:04 +0200

    test

--- world/server.js
+++ world/server.js
@@ -6,11 +6,4 @@ app.get('/world', (req, res) => res.send('World'));
 
 app.use('/world/files', express.static('files'));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-process.on('SIGTERM', () => {
-    console.log('SIGTERM signal received: closing HTTP server in 10 seconds')
-
-    const closeServer = () => server.close()
-    setTimeout(closeServer, 10000)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
